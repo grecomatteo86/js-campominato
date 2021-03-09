768e2055610dbc427ad72b364be19c1ce5ff762a
Matteo <grecomatteo86@gmail.com>
2021-03-09 15:21:46 +0100

    imposto condizioni di sconfitta

--- js/script.js
+++ js/script.js
@@ -13,9 +13,17 @@ for ( i = 0 ; i < 8; i++) {
 }
 
 // stampo il mio array con gli 8 numeri random
-console.log(listaNera);
+console.log('i numeri vietati sono ' + listaNera);
 
 
 // chiedo all'utente di inseriree un numero compreso tra
 var numeroUtente = parseInt(prompt('inserisci un numero compreso tra'));
 console.log('il numero inserito dall\'utente è ' + numeroUtente);
+
+
+// eseguo il controllo: se il numero inserito dall'utente è presente nella lista di numeri random allora l'utente ha perso, altrimenti continua a giocare
+if (listaNera.includes(numeroUtente)) {
+  alert('hai perso');
+} else {
+  alert('continua a giocare');
+}
