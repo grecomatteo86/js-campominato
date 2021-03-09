2eefb4d4ef7cc4e887c854da18f8ea85bfa3e8f0
Matteo <grecomatteo86@gmail.com>
2021-03-09 15:15:30 +0100

    creazione array vuoto e push numeri random dentro array

--- js/script.js
+++ js/script.js
@@ -3,14 +3,19 @@ function randomizzatore(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+//creo un array vuoto che sarà il mio 'array numeri vietati'
+var listaNera = [];
 
 // con un ciclo for genero 8 numeri random e al suo interno invoco la funzione 'randomizzatore'
 for ( i = 0 ; i < 8; i++) {
   var numeroRandom = randomizzatore (1,50);
-  console.log(numeroRandom);
+  listaNera.push(numeroRandom);
 }
+
+// stampo il mio array con gli 8 numeri random
+console.log(listaNera);
 
 
 // chiedo all'utente di inseriree un numero compreso tra
 var numeroUtente = parseInt(prompt('inserisci un numero compreso tra'));
-console.log(numeroUtente);
+console.log('il numero inserito dall\'utente è ' + numeroUtente);
