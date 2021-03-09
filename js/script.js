b7221d674b4b5eadcf0360541aad916c155fd94d
Matteo <grecomatteo86@gmail.com>
2021-03-09 15:03:56 +0100

    creazione ciclo for per generare i numeri random

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@
 function randomizzatore(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+
+// con un ciclo for genero 8 numeri random e al suo interno invoco la funzione 'randomizzatore'
+for ( i = 0 ; i < 8; i++) {
+  var numeroRandom = randomizzatore (1,50);
+  console.log(numeroRandom);
+}
