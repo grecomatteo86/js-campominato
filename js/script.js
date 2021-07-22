35d965fbde8a26ee2d9b1597ae270ee507615ec9
Matteo <grecomatteo86@gmail.com>
2021-07-22 13:06:44 +0200

    fourth fix: point 2

--- js/script.js
+++ js/script.js
@@ -54,51 +54,31 @@ while (bombsArray.length < 16) {
     bombsArray.push(randomNumber);
   }
 }
-
-/* 2. Chiedo all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-L’utente non può inserire più volte lo stesso numero.
-Se il numero è presente nella lista delle bombe, la partita termina;
-altrimenti si continua chiedendo all’utente un altro numero. */
-
-var numeriValidi = [];  //la creazione di questo array mi serve per salvarmi i numeri (validi) che man mano sceglie l'utente e per dire al programma che l'utente non può scegliere numeri già scelti
-var bombaEspolsa = false;
-
-/* Faccio un ciclo while: non vado a chiedere all'utente di inserire per 84 volte il numero, ma gli chiedo i numeri SOLO non sono numeri bomba e non sono presenti nell'array numeriValidi; ecco perchè il contatore del mio ciclo while sarà numeriValidi.length < 84;
-Questo numeriValidi.length < 84 risponde alla logica che la partita termina quando il giocatore inserisce un numero “vietato” o RAGGIUNGE IL MASSIMO POSSIBILE DEI NUMERI CONSENTITI, che sono 84.
-Ora, invece chiedo < 84 inizialmente metto 10 perchè mi consente di fare il debug in maniera più facile. */
-
-var possibilità = limMaxNum - 16;   //la var possibilità mi va a cambiare l'iniziale valore di 84 (100 - 16), che deve cambiare in base alla scelta del livello dell'utente
-
-while (numeriValidi.length < possibilità && bombaEspolsa == false) {
-
-  var numero = parseInt(prompt('inserisci un numero'));
-
-  if (isNaN(numero) || numero < 1 || numero > limMaxNum) {      //controllo: il numero inserito dall'utente deve essere compreso tra 1 e 100.
-
-    alert('ATTENZIONE: quello che inserisci deve essere un NUMERO e deve essere COMPRESO tra 1 e ' + limMaxNum);
-
-  } else if (inArray(bombsArray,numero) == true) {     //controllo:se il numero inserito dall'utente è una bomba allora la partita deve terminare;come faccio a uscire dal ciclo se l'utente becca la bomba?con lo stratagemma della var d'appoggio:mi creo la var bombaEspolsa, inizialmente impostata false;la inserisco nel while, come condizione di prosecuzione del mio ciclo e dentro l'if dico che se l'utente inserisce un numero bomba allora questa var diventa true; in questo modo mi fa terminare il ciclo
-
-    bombaEspolsa = true;
-
-  } else if (inArray(numeriValidi,numero) == true) {    //controllo: se l'utente inserisce un numero CONTENUTO nell'array numeriValidi, allora faccio un alert dove gli dico di reinserire il numero
-
-    alert('Non fare il FURBO, inserisci un numero che non avevi scelto prima');
-
-  } else {     //controllo: altrimenti fai il push del numero nella lista numeriValidi
-
-    numeriValidi.push(numero);
-
+// 2. I ask to the user to insert a number. User CANNOT INSERT THE SAME NUMBER TWICE.
+// If userNumber is in bombsArray, the game ends, otherwise go on asking another number.
+var bombsFreeArray = [];
+var pickedBomb = false;
+var userPossibilities = limMaxNum - 16;
+while (bombsFreeArray.length < userPossibilities && pickedBomb == false) {
+  var userNumber = parseInt(prompt('Please, insert a number'));
+  if (isNaN(userNumber) || userNumber < 1 || userNumber > limMaxNum) {
+    alert('PAY ATTENTION: your number MUST BE between 1 and ' + limMaxNum);
+  } else if (inArray(bombsArray,userNumber) == true) {
+    pickedBomb = true;
+  } else if (inArray(bombsFreeArray,userNumber) == true) {
+    alert('PAY ATTENTION: YOU CANNOT INSERT THE SAME NUMBER TWICE');
+  } else {
+    bombsFreeArray.push(userNumber);
   }
-
 }
+
 
 /* 3. La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito. */
 
-if (bombaEspolsa == true) {  //controllo: a questo punto del programma vado a verificare il valore della var bombaEspolsa, perchè se uguale a true vuol dire che l'utente ha beccato una bomba
+if (pickedBomb == true) {  //controllo: a questo punto del programma vado a verificare il valore della var bombaEspolsa, perchè se uguale a true vuol dire che l'utente ha beccato una bomba
 
-  console.log('Hai preso la BOMBA!!! Hai totalizzato ' + numeriValidi.length + ' punti')
+  console.log('Hai preso la BOMBA!!! Hai totalizzato ' + bombsFreeArray.length + ' punti')
 
 } else {   //se arrivo fin qui vuol dire che l'utente ha indovinato tutti i numeri consentiti e non ha preso nessuna bomba, quindi il valore della var bombaEspolsa è rimasto false
 
